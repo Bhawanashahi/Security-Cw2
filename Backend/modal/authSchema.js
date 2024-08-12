f051bbb3b3be25f570fd5188b724d3f494099e0b
Bhawana shahi <shahivawana61@gmail.com>
2024-08-12 16:31:44 +0545

    add auth schema

--- Backend/modal/authSchema.js
+++ Backend/modal/authSchema.js
@@ -1,39 +1,29 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
 const authSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
   },
-
   password: {
     type: String,
     required: true,
   },
-
   role: {
     type: String,
   },
-
   otp: {
     type: Number,
   },
   expireOtp: {
     type: Number,
   },
-
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "USER",
   },
-
   adminId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "ADMIN",
   },
-
   tokens: [
     {
       token: {
@@ -41,29 +31,17 @@ const authSchema = new mongoose.Schema({
       },
     },
   ],
+  passwordLastChanged: {
+    type: Date,
+    default: Date.now, // Set the default to the current date
+  },
 });
 
-// hashing password
+// Hashing password
 authSchema.pre("save", async function (next) {
-  console.log("I am inside");
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 12);
+    this.passwordLastChanged = Date.now(); // Update the password last changed date
   }
   next();
 });
-
-// we are generating token
-authSchema.methods.generateAuthToken = async function () {
-  try {
-    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
-    this.tokens = this.tokens.concat({ token: token });
-    await this.save();
-    return token;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const Auths = mongoose.model("AUTH", authSchema);
-
-module.exports = Auths;
